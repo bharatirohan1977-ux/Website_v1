db538d205c084ce13c7528ef41243d6c486bba16
bharatirohan1977-ux <bharatirohan1977@gmail.com>
2026-05-02 19:21:17 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Website_v1/',
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
